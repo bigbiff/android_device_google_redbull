938e8042889f6c96a215fe4b48261fcf1cead88a
bigbiff <bigbiff@teamw.in>
2021-07-31 21:16:47 -0400

    redbull: use vendor touch modules when recovery in boot

--- prebuilts/touchdriver.sh
+++ prebuilts/touchdriver.sh
@@ -1,8 +1,21 @@
 #!/system/bin/sh
 
 fastboot=$(getprop ro.boot.bootreason | cut -d, -F2)
-if [[ $fastboot == "bootloader" || $fastboot == "longkey" || $fastboot == "reboot"  || $fastboot == "recovery" || $fastboot == "reboot,recovery" ]]
+if [[ $fastboot == "bootloader" || $fastboot == "longkey" || $fastboot == "reboot" ]]
 then
+	insmod /ftm5.ko
+	insmod /sec_touch.ko
+fi
+if [[ $fastboot == "recovery" || $fastboot == "reboot,recovery" ]]
+then
+	twrp mount vendor
+	cp /vendor/lib/modules/heatmap.ko /
+	cp /vendor/lib/modules/touch_offload.ko /
+	cp /vendor/lib/modules/ftm5.ko /
+	cp /vendor/lib/modules/sec_touch.ko /
+	twrp unmount vendor
+	insmod /heatmap.ko
+	insmod /touch_offload.ko
 	insmod /ftm5.ko
 	insmod /sec_touch.ko
 fi
